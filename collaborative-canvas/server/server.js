00037c0322b68cd6e13e0e6757643a1d0b34a709
Sanchayanpersonal-07 <sanchayansoumi2004@gmail.com>
2026-01-17 20:53:19 +0530

    Listen fix

--- collaborative-canvas/server/server.js
+++ collaborative-canvas/server/server.js
@@ -9,10 +9,6 @@ const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 const io = new Server(server, {
   cors: {
     origin: "*",
